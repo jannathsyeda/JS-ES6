c89ba16cdc6bccd3e255b9e9f7b531d6882f7c57
jannath <jannathsyeda7@gmail.com>
2021-02-07 15:58:46 +0600

    arrow function

--- ES6.js
+++ ES6.js
@@ -28,4 +28,69 @@ for(let i=0 ;i<10;i++)
     sum=sum+i;
    
 }
-console.log(i);
+// console.log(i);//it show eeror undefined ,cz let is use in scope,inside the curly basis
+
+
+//default pereameter
+//if default is declare ,and also pass peram ,then ignor default
+function add(n1,n2=20)
+{
+    sum=n1+n2;
+    return sum;
+}
+let result=add(1,3);
+console.log(result);
+
+
+//es6 template
+//* use kharet(``), *can see multi line with template
+
+const firstName="fahima";
+
+const lastName="jannath";
+const add2=`${firstName} ${lastName} ${1+6+8} u can use karet  ,it called es6 template.it is usefull cz it ignore adding strings without + and all `;
+console.log(add2);
+
+//*can see multi line with template
+
+const multiline=`1multiline.
+2multiline.
+3multiline.`;
+console.log(multiline);
+
+
+//old function
+const doubleIt=function myfun(num) {
+
+    let sum1=0;
+    sum1=sum1+num;
+    return num*2 ,sum1;
+}
+const sum1=doubleIt(10);
+console.log(' result :' ,sum1);
+
+// ES6 arrow function 
+const  arrowFunction= n1 => n1*10;
+const resultArrow=arrowFunction(5);
+console.log(resultArrow);
+
+//two param
+const  arrowFunction2= (n1,n2) => n1*n2;
+const resultArrow2=arrowFunction2(5,7);
+console.log(resultArrow2);
+
+//without param
+const  arrowFunction3 = () => 5;
+const resultArrow3=arrowFunction3();
+console.log(resultArrow3);
+
+//arrow
+const doMath=(x,y) =>{
+    const sum=x+y;
+    const diff=x-y;
+    const result= sum*diff;
+    return result;
+
+}
+let result6=doMath(7,4);
+console.log(result6);
